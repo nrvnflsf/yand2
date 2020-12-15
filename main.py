97c0e56a003d286fd99bc8302f802968035d4491
p <nrvnflsf@gmail.com>
2020-12-15 20:55:45 +0300

    1

---
+++ main.py
@@ -0,0 +1,35 @@
+import sys
+from random import randint
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtGui import QPainter, QColor
+
+
+class MyWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('UI.ui', self)
+        self.pushButton.clicked.connect(self.run)
+        self.do_paint = False
+
+    def run(self):
+        self.do_paint = True
+        self.repaint()
+
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            for i in range(randint(5, 15)):
+                qp.setBrush(QColor('yellow'))
+                n = randint(5, 300)
+                qp.drawEllipse(randint(1, 1200), randint(1, 800), n, n)
+            qp.end()
+            self.do_paint = False
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec_())
